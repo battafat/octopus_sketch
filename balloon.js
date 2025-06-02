015b67f96112d1c9fab13ff4382e326bd66b7ca7
timothy battafarano <tim@deepsprings.edu>
2025-06-02 15:44:57 -0500

    made balloon class and included balloon.js in index.html

---
+++ balloon.js
@@ -0,0 +1,17 @@
+class attachmentPoint{
+
+}
+
+class Balloon {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    show() {
+        fill(255, 100, 150);
+        ellipse(this.x, this.y, 150, 150);
+    }
+}
+class tentacle {
+
+}
--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
+let balloon;
+
 function setup() {
   createCanvas(400, 400);
+  balloon = new Balloon(width / 2, height / 2 - 50);
 }
 
 function draw() {
-  background("aqua");
+  background(200);
+  balloon.show();
 }
