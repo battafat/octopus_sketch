ea91ad6446670596a87f6f02dd3dc67e7bc67859
timothy battafarano <tim@deepsprings.edu>
2025-06-03 15:55:44 -0500

    separated the model/data logic from the view/render logic in case I change libraries from p5.js at some point

--- balloon.js
+++ balloon.js
@@ -6,12 +6,31 @@ class Balloon {
     constructor(x, y) {
         this.x = x;
         this.y = y;
+
+        // Create one tentacle hanging from the bottom-center of the balloon
+        this.tentacle = new Tentacle(this.x, this.y + 75);
     }
+
     show() {
         fill(255, 100, 150);
+        noStroke();
         ellipse(this.x, this.y, 150, 150);
+
+        // Show the tentacle
+        this.tentacle.show();
+    }
+  }
+class Tentacle {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.length = 60;
+    }
+
+    show() {
+        stroke(0);
+        strokeWeight(4);
+        line(this.x, this.y, this.x, this.y + this.length);
     }
 }
-class tentacle {
-
-}
+  
